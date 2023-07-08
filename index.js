fc858ef97d9b52268dd2c2116a6ac6f9224dbee1
max <maxmakarow86@gmail.com>
2023-07-08 11:10:08 +0300

    data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const jsonServer = require("json-serve");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares= jsonServer.defaults();
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares)
 server.use(router)
 
-server.listen(port)
+server.listen(port )
